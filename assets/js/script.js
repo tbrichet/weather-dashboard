fbb8c131ef08b04f065c5b1cfdd08966198efca2
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-09-06 19:33:24 -0700

    dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,13 @@ var userFormEl = document.querySelector("#form");
 //Reference HTML Form Input
 var nameInputEl = document.querySelector("#name-input");
 
-//Date Variable
+//Date Variables
 var currentDay = moment().format("MM/DD/YYYY")
+var dayOne = moment().add(1, 'days').format("MM/DD/YYYY");
+var dayTwo = moment().add(2, 'days').format("MM/DD/YYYY");
+var dayThree = moment().add(3, 'days').format("MM/DD/YYYY");
+var dayFour = moment().add(4, 'days').format("MM/DD/YYYY");
+var dayFive = moment().add(5, 'days').format("MM/DD/YYYY");
 
 
 //Function Button Click Handler
@@ -19,17 +24,41 @@ var formSubmitHandler = function(event) {
     if(cityName) {
         displayCityName(cityName, currentDay);
         nameInputEl.value="";
+        forecast();
     } else {
         alert("Apologies, the city name you have entered is not in our database.")
     }
 };
 
-//Function to Display City Name
+//Function to Display Searched City Name and Today's Date
 var displayCityName = function(cityName, currentDay) {
-    var cityNameDisplayEl = document.getElementById('city-name-display')
+    var cityNameDisplayEl = document.getElementById('city-name-display');
     cityNameDisplayEl.innerHTML = "<h3>" + cityName + " (" + currentDay + ") " + "</h3>";
 };
 
+//Function to Display Forecast Dates
+var forecast = function() {
+    //Day 1
+    var displayDayOne = document.getElementById('dayone');
+    displayDayOne.innerHTML = dayOne
+
+    //Day 2
+    var displayDayTwo = document.getElementById('daytwo');
+    displayDayTwo.innerHTML = dayTwo
+
+    //Day 3
+    var displayDayThree = document.getElementById('daythree');
+    displayDayThree.innerHTML = dayThree
+
+    //Day 4
+    var displayDayFour = document.getElementById('dayfour');
+    displayDayFour.innerHTML = dayFour
+
+    //Day 5
+    var displayDayFive = document.getElementById('dayfive');
+    displayDayFive.innerHTML = dayFive
+    
+}
 
 //Event Handler for Submit Button
 userFormEl.addEventListener("submit", formSubmitHandler);
